cec5baa009d8ad5994d8cf50c48f6db0d714af74
Toni Skoro <skoro.toni01@gmail.com>
2025-01-15 12:23:47 +0100

    Connected to Spring Boot

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
   // Function to handle the POST request
   const handlePostRequest = () => {
     // Make a POST request to the Spring Boot server
-    axios.post('http://localhost:8080/')
+    axios.post('https://e-commerce-app-backend-vert.vercel.app/')
       .then(response => {
         setResponseMessage(response.data); // Set the response message from the backend
       })
